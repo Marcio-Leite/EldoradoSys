4abc26c230668d67ebad2711b00c5f71b7205cd1
Marcio Leite <marciovinicius.leite@hotmail.com>
2020-08-20 23:47:26 -0300

    Funcionando

---
+++ EldoradoService/Migrations/20200821024122_inicio.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EldoradoService.Migrations
+{
+    public partial class inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationRoles",
+                columns: table => new
+                {
+                    ApplicationRoleId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationRoles", x => x.ApplicationRoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUsers",
+                columns: table => new
+                {
+                    ApplicationUserId = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUsers", x => x.ApplicationUserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserRoles",
+                columns: table => new
+                {
+                    ApplicationRoleId = table.Column<Guid>(nullable: false),
+                    ApplicationUserId = table.Column<Guid>(nullable: false),
+                    ApplicationRoleId1 = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserRoles", x => x.ApplicationRoleId);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserRoles_ApplicationRoles_ApplicationRoleId1",
+                        column: x => x.ApplicationRoleId1,
+                        principalTable: "ApplicationRoles",
+                        principalColumn: "ApplicationRoleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserRoles_ApplicationUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "ApplicationUsers",
+                        principalColumn: "ApplicationUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserRoles_ApplicationRoleId1",
+                table: "ApplicationUserRoles",
+                column: "ApplicationRoleId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserRoles_ApplicationUserId",
+                table: "ApplicationUserRoles",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationRoles");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUsers");
+        }
+    }
+}
--- EldoradoService/Startup.cs
+++ EldoradoService/Startup.cs
@@ -29,7 +29,8 @@ namespace EldoradoService
         {
             services.AddControllers();
             services.AddDbContext<PersistenceDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")
+                    , b => b.MigrationsAssembly("EldoradoService")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
